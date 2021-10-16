d30c8ec90eb2010bb13d53fb2d33fa12645dcec9
juunini <juuni.ni.i@gmail.com>
2021-10-16 16:35:16 +0900

    Add manifest.json

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const isProduction = process.env.NODE_ENV === 'production';
-const productionURL = 'https://your.production.site/';
+const productionURL = 'https://sex-request.github.io/nextjs-starter-kit/';
 const withMDX = require('@next/mdx')({
   extension: /\.mdx$/,
 });
